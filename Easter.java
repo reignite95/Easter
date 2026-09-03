29439c37bb0f7eb9e34898a3113bcd48659c9b42
reignite95 <rehank1800@gmail.com>
2026-09-03 14:53:40 +0000

    started easter project

--- Easter.java
+++ Easter.java
@@ -1,1 +1,14 @@
+// Rehan Khan
+// This program will calculate the date easter falls on given a year after 1583.
+import java.util.*;
+
+public class Easter{
+    public static void main(String[] args) {
+        //code goes here
+        Scanner scanner = new Scanner(System.in); //this allows you to read input from the user.
+        int y = scanner.nextInt(); // reads the next number the user types. 
+        int a = y % 19;
+        System.out.println("a = "+a);
+    }
+}
 
