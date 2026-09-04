0c26e103b74a04c5a5882dcc00ed69e87ea77be6
reignite95 <rehank1800@gmail.com>
2026-09-04 14:51:50 +0000

    easter final

--- Easter.java
+++ Easter.java
@@ -9,6 +9,36 @@ public class Easter{
         int y = scanner.nextInt(); // reads the next number the user types. 
         int a = y % 19;
         System.out.println("a = "+a);
+        int b = y / 100;
+        int c = y % 100;
+        System.out.println("b = "+b);
+        System.out.println("c = "+c);
+        int d = b / 4;
+        int e = b % 4;
+        System.out.println("d = "+d);
+        System.out.println("e = "+e);
+        int f = ((b + 8) / 25);
+        System.out.println("f = "+f);
+        int g = (b - f + 1)/3;
+        System.out.println("g = "+g);
+        int h = ((19 * a + b - d - g + 15) % 30);
+        System.out.println("h = "+h);
+        int i = c / 4;
+        int k = c % 4;
+        System.out.println("i = "+i);
+        System.out.println("k = "+k);
+        int r = (32 + 2 * e + 2 * i - h - k) % 7;
+        System.out.println("r = "+r);
+        int m = (a + 11 * h + 22 * r)/451;
+        System.out.println("m = "+m);
+        int n = (h + r - 7 * m + 114) / 31;
+        int p = (h + r - 7 * m + 114) % 31;
+        System.out.println("n = "+n);
+        System.out.println("p = "+p);
+        int easterDay = p + 1;
+
+        //Prints easter date
+        System.out.println("\nEaster in "+ y + " falls on " + n + "/" + easterDay);
     }
 }
 
